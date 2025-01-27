f79f1905fabfcf0d474289a5d58d56f22b6e56b6
dmaresza <daniel.a.mares.z@gmail.com>
2025-01-27 14:49:53 -0500

    Added title and description to game

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -39,6 +39,8 @@ export default function App() {
 
   return (
     <main>
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceComponents}
       </div>
