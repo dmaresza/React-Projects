0886bb7fc63525e1690f3aab53f940202f220e5a
dmaresza <daniel.a.mares.z@gmail.com>
2025-01-27 14:05:39 -0500

    Added functionality to reroll dice

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -3,14 +3,27 @@ import { useState } from "react"
 
 export default function App() {
 
-  const [dice, setDice] = useState([2, 2, 3, 4, 5, 6, 5, 4, 3, 2])
+  const [dice, setDice] = useState(generateAllNewDice())
   const diceComponents = dice.map((die, index) => <Die key={index} value={die} />)
+
+  function generateAllNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice())
+  }
 
   return (
     <main>
       <div className="dice-container">
         {diceComponents}
       </div>
+      <button onClick={rollDice} className="roll-button">Roll</button>
     </main>
   )
 }
