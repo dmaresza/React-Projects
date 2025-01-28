7bd3ebe62336dff526751f624841cb0124db4bd6
dmaresza <daniel.a.mares.z@gmail.com>
2025-01-28 10:49:43 -0500

    Drop confetti when game is won, give choice for new game

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,10 +1,12 @@
 import Die from "./Die.jsx"
 import { useState } from "react"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
 
-  const [dice, setDice] = useState(generateAllNewDice())
+  // Constants
+  const [dice, setDice] = useState(() => generateAllNewDice())
   const diceComponents = dice.map(die => (
     <Die
       key={die.id}
@@ -12,7 +14,9 @@ export default function App() {
       isHeld={die.isHeld}
       hold={() => hold(die.id)}
     />))
+  const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
 
+  // Functions
   function generateAllNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
@@ -37,14 +41,24 @@ export default function App() {
     )
   }
 
+  // JSX elements
   return (
     <main>
+      {gameWon && <Confetti />}
+      <div aria-live="polite" className="sr-only">
+        {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+      </div>
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceComponents}
       </div>
-      <button onClick={rollDice} className="roll-button">Roll</button>
+      <button
+        onClick={gameWon ? () => setDice(generateAllNewDice()) : rollDice}
+        className="roll-button"
+      >
+        {gameWon ? "New Game" : "Roll"}
+      </button>
     </main>
   )
 }
--- tenzies/src/Die.jsx
+++ tenzies/src/Die.jsx
@@ -1,9 +1,10 @@
 export default function Die(props) {
     return (
         <button
-            // key={props.key}
             onClick={props.hold}
             className={props.isHeld ? "clicked" : "unclicked"}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, ${props.isHeld ? "held" : "not held"}`}
         >{props.value}</button>
     )
 }
