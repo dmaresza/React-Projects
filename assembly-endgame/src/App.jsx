b7b3cb891e9f193d0ecc4133a49390d08b787469
dmaresza <daniel.a.mares.z@gmail.com>
2025-01-29 13:23:09 -0500

    Added game title, description, and status sections

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -3,8 +3,15 @@ import { useState } from 'react'
 export default function App() {
 
   return (
-    <>
-
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className="status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+    </main>
   )
 }
